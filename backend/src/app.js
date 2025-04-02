ffabf5fc09203d0ab24ae47db9fef193c81d460b
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-04-02 12:01:10 +0530

    Add cookie-parser middleware and refactor error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-import errorHandler from "./middlewares/errorMiddlewar.js";
-import notFound from "./middlewares/notFoundMiddleware.js";
+import errorHandler from "./middlewares/errorMiddleware.js";
+// import notFound from "./middlewares/notFoundMiddleware.js";
 import taxRoutes from "./routes/taxRoutes.js";
 import bannerRoutes from "./routes/bannerRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-
+import fileUpload from "express-fileupload";
+import cookieParser from "cookie-parser";
 const app = express();
 
 // Load environment variables from .env
@@ -18,14 +19,15 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-
+app.use(fileUpload());
+app.use(cookieParser());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tax", taxRoutes);
 app.use("/api/banner", bannerRoutes);
 app.use("/api/admin", adminRoutes);
 // Handle Undefined Routes
-app.use(notFound);
+// app.use(notFound);
 // Global Error Handler Middleware (Should be placed last)
 app.use(errorHandler);
 
