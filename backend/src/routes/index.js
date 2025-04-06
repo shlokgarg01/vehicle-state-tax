0dd5056c0d6ef7cf43700d0a66aef22de2285408
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-04-06 09:06:24 +0530

    little changes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,7 +5,6 @@ import taxRoutes from "./taxRoutes.js";
 import bannerRoutes from "./bannerRoutes.js";
 import adminRoutes from "./adminRoutes.js";
 import stateRoutes from "./stateRoutes.js";
-import seatTypeRoutes from "./seatTypesRoutes.js";
 
 const router = express.Router();
 
@@ -14,6 +13,5 @@ router.use("/tax", taxRoutes);
 router.use("/banner", bannerRoutes);
 router.use("/admin", adminRoutes);
 router.use("/state", stateRoutes);
-router.use("/seat-types", seatTypeRoutes);
 
 export default router;
