3380eaa2e49711c950c99265840e3e91d7c583ac
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-04-07 12:42:02 +0530

    all constants add

--- backend/src/constants/constants.js
+++ backend/src/constants/constants.js
@@ -1,23 +1,74 @@
-export const CONSTANTS = {
+const CONSTANTS = {
   ORDER_STATUS: {
     CREATED: "created",
     CONFIRMED: "confirmed",
     CLOSED: "closed",
     CANCELLED: "cancelled",
   },
+
   PAYMENT: {
     TRANSACTION_STATUS: {
       SUCCESS: "SUCCESS",
     },
   },
+
   TAX_CATEGORIES: {
-    ROAD_TAX: "border_tax",
-    BORDER_TAX: "road_tax",
+    ROAD_TAX: "road_tax",
+    BORDER_TAX: "border_tax",
     ALL_INDIA_PERMIT: "all_india_permit",
     ALL_INDIA_TAX: "all_india_tax",
     LOADING_VEHICLE: "loading_vehicle",
   },
+
+  STATUS: {
+    ACTIVE: "active",
+    INACTIVE: "inactive",
+  },
+
+  USER_ROLES: {
+    ADMIN: "admin",
+    MANAGER: "manager",
+  },
+
+  MODES: {
+    ROAD: "road",
+    BORDER: "border",
+    ALL_INDIA: "allIndia",
+    LOADING_VEHICLE: "loadingVeh",
+  },
+
+  TAX_MODES: {
+    YEARLY: "yearly",
+    QUARTERLY: "quarterly",
+    MONTHLY: "monthly",
+    WEEKLY: "weekly",
+    DAILY: "daily",
+    CUSTOM: "custom",
+  },
+
+  SEAT_CAPACITY: {
+    FOUR_PLUS_ONE: "4+1",
+    FIVE_PLUS_ONE: "5+1",
+    SIX_PLUS_ONE: "6+1",
+    SEVEN_PLUS_ONE: "7+1",
+  },
+
+  VEHICLE_TYPES: {
+    LIGHT: "light goods vehicle",
+    MEDIUM: "medium goods vehicle",
+    HEAVY: "heavy goods vehicle",
+  },
+
+  // TAX_MODELS: {
+  //   BorderTax: "BorderTax",
+  //   RoadTax: "RoadTax",
+  //   AllIndiaPermit: "AllIndiaPermit",
+  //   AllIndiaTax: "AllIndiaTax",
+  //   LoadingVehicle: "LoadingVehicle",
+  // },
 };
+
+export default CONSTANTS;
 export const STATUS = {
   ACTIVE: "active",
   INACTIVE: "inactive",
